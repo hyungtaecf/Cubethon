c7fb36d26455dd1481e963312967a1fdc5a53e86
Hyung Tae C. Figur <hyu_03@hotmail.com>
2019-02-07 21:58:43 +0900

    Changing few things
    playability

---
+++ Assets/Scripts/ObstacleCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public AudioSource _as;
+    public float pitchDecreasingLevel = 0.5f;
+
+    private bool collided;
+
+
+    void Start()
+    {
+        _as = GetComponent<AudioSource>();
+        collided = false;
+    }
+
+    void Update()
+    {
+        if (collided)
+        {
+            _as.pitch -= pitchDecreasingLevel*Time.deltaTime;
+            Debug.Log(_as.pitch.ToString());
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag.ToString() == "Player")
+        {
+            Debug.Log("player collided with one object");
+            collided = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,11 +6,13 @@ public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement;
     public Score score;
+    public AudioClip collisionSound;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
+            GetComponent<AudioSource>().PlayOneShot(collisionSound);
             movement.enabled = false;
             score.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
